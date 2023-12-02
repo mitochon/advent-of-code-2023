e837516c1d79cefa6d91e1911d6672a57fc91f5d
Francisco Tanudjaja <francisco.tanudjaja@helix.com>
2023-12-01 23:38:27 -0800

    day2

---
+++ src/main/scala/aoc/Day2.scala
@@ -0,0 +1,58 @@
+package aoc
+
+import java.io.File
+import java.nio.file.Paths
+import scala.io.Source
+
+object Day2 extends App {
+
+  private val redRegex = """(\d+) red""".r
+  private val blueRegex = """(\d+) blue""".r
+  private val greenRegex = """(\d+) green""".r
+  private val gameRegex = """Game (\d+): (.+)""".r
+
+  /* Red Blue Green */
+  case class RBG(r: Int, b: Int, g: Int) {
+    def power(): Int = r * b * g
+    def merge(other: RBG): RBG = RBG(math.max(this.r, other.r), math.max(this.b, other.b), math.max(this.g, other.g))
+  }
+  object RBG {
+    def fromGameLine(line: String): RBG = line.split("[;]").map(RBG(_)).foldLeft(RBG(0, 0, 0))(_.merge(_))
+    def apply(line: String): RBG = {
+      line.split("[,]")
+        .map(_.trim)
+        .foldLeft(RBG(0, 0, 0)) {
+          case (rbg, entry) =>
+            entry match {
+              case redRegex(n) => rbg.copy(r = n.toInt)
+              case blueRegex(n) => rbg.copy(b = n.toInt)
+              case greenRegex(n) => rbg.copy(g = n.toInt)
+              case _ => throw new IllegalArgumentException(s"Invalid line: $line")
+            }
+        }
+    }
+  }
+
+  case class Game(id: Int, rbg: RBG) {
+    def checkPossible(constraint: RBG): Boolean = rbg.r <= constraint.r && rbg.b <= constraint.b && rbg.g <= constraint.g
+  }
+  object Game {
+    def apply(line: String): Game = {
+      line match {
+        case gameRegex(id, rest) => Game(id.toInt, RBG.fromGameLine(rest))
+        case _ => throw new IllegalArgumentException(s"Invalid line: $line")
+      }
+    }
+  }
+
+  def getLines(file: File) = Source.fromFile(file).getLines()
+  val file = Paths.get("src/main/resources/day2/input").toFile
+
+  val constraint = RBG(12, 14, 13)
+  val games = getLines(file).map(Game(_)).toList
+  val powers = games.map(_.rbg.power())
+  val possibleGames = games.filter(_.checkPossible(constraint))
+
+  println(possibleGames.map(_.id).sum)
+  println(powers.sum)
+}
