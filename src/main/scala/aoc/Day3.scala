eb71cdc5cec4a59b8c8a8fad40cff37f428fc1ac
Francisco Tanudjaja <francisco.tanudjaja@helix.com>
2023-12-04 00:30:45 -0800

    fixes, tidy up

--- src/main/scala/aoc/Day3.scala
+++ src/main/scala/aoc/Day3.scala
@@ -1,21 +1,21 @@
 package aoc
 
+import scala.annotation.tailrec
+
 object Day3 extends App {
 
   val numberRegex = """(\d+)""".r
 
   case class Coord(row: Int, col: Int)
-
   case class Symbol(s: Char, c: Coord) {
     def adjacentCoords(): Seq[Coord] = {
       (-1 to 1).flatMap { x =>
         (-1 to 1).map { y =>
           Coord(c.row + x, c.col + y)
         }
-      }//.filter(_.row >= 0).filter(_.col >= 0).filterNot(_ == c)
+      }
     }
   }
-
   case class Part(n: Int, c: Coord) {
     def coords(): Seq[Coord] = (0 until n.toString.size).map { i => Coord(c.row, c.col + i) }
   }
@@ -27,30 +27,39 @@ object Day3 extends App {
       .map { case (s, col) => Symbol(s, Coord(row, col)) }
   }
 
-  def parseParts(line: String, row: Int): Seq[Part] = {
+  def parseParts2(line: String, row: Int): Seq[Part] = {
     val numbers = numberRegex.findAllIn(line).map(_.toInt).toSeq
     val indices = numbers.map(n => line.indexOf(n.toString))
     numbers.zip(indices).map { case (n, col) => Part(n, Coord(row, col)) }
   }
 
-//  val file = getResourceFile("day3/inputPart1")
-    val file = getResourceFile("day3/input")
+  def parseParts(line: String, row: Int): Seq[Part] = {
+    def makePart(digits: String, index: Int) = Part(digits.toInt, Coord(row, index + 1 - digits.size))
+    @tailrec
+    def build(acc: List[Part], digits: String, lastIndex: Int, remainder: List[(Char, Int)]): List[Part] = {
+      remainder match {
+        case Nil if digits.isEmpty => acc
+        case Nil => makePart(digits, lastIndex) :: acc
+        case (c, colIndex) :: xs if digits.isEmpty || lastIndex + 1 == colIndex => build(acc, digits + c, colIndex, xs)
+        case (c, colIndex) :: xs => build(makePart(digits, lastIndex) :: acc, "" + c, colIndex, xs)
+      }
+    }
+    // initial recursive call
+    build(Nil, "", -1, line.zipWithIndex.filter(_._1.isDigit).toList).reverse
+  }
 
+  val file = getResourceFile("day3/input")
   val symbols = getLines(file).zipWithIndex.flatMap {
     case (line, row) => parseSymbols(line, row)
   }
-
-  val partsCoordinates = symbols.flatMap(_.adjacentCoords()).toSet
-
   val parts = getLines(file).zipWithIndex.flatMap {
     case (line, row) => parseParts(line, row)
   }
 
-//  parts.foreach(p => {println(p); println(p.coords())})
+  val validCoords = symbols.flatMap(_.adjacentCoords()).toSet
+  val validParts = parts.filter(p => p.coords().exists(c => validCoords.contains(c))).toList
+  // debug
+  validParts.groupBy(_.c.row).toList.sortBy(_._1).foreach { case (row, parts) => println(s"$row: ${parts.map(_.n)}") }
 
-  val filteredParts = parts.filter(p => p.coords().exists(c => partsCoordinates.contains(c)))
-  println(filteredParts.map(_.n).sum)
-
-  // 521242
-  //That's not the right answer; your answer is too low. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again. [Return to Day 3]
+  println(validParts.map(_.n).sum)
 }
