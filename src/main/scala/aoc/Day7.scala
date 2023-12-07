10a5c5109cb8831ae967666eba6a6d3443b1ddd7
Francisco Tanudjaja <francisco.tanudjaja@helix.com>
2023-12-06 23:59:03 -0800

    day7 part2

--- src/main/scala/aoc/Day7.scala
+++ src/main/scala/aoc/Day7.scala
@@ -5,41 +5,40 @@ object Day7 extends App {
   val labels = List('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2').reverse
   val labelPoints = labels.zipWithIndex.toMap
 
-  case class CamelCard(hand: String, bid: Int) extends Ordered[CamelCard] {
+  val labelsWithJoker = List('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J').reverse
+  val labelPointsWithJoker = labelsWithJoker.zipWithIndex.toMap
 
-    def getKind(): Int = {
-      hand.groupBy(identity).view
-        .mapValues(_.size)
-        .values.toList
-        .sorted.reverse match {
-          case 5 :: xs => 6
-          case 4 :: xs => 5
-          case 3 :: 2 :: xs => 4
-          case 3 :: xs => 3
-          case 2 :: 2 :: xs => 2
-          case 2 :: xs => 1
-          case _ => 0
-        }
+  case class CamelCard(hand: String, bid: Int) {
+
+    def getKind(): Int = getKind(CamelCard.getCounts(hand))
+
+    def getKind(counts: List[Int]): Int = {
+      counts match {
+        case 5 :: xs => 6
+        case 4 :: xs => 5
+        case 3 :: 2 :: xs => 4
+        case 3 :: xs => 3
+        case 2 :: 2 :: xs => 2
+        case 2 :: xs => 1
+        case _ => 0
+      }
     }
 
-    def isStronger(other: CamelCard): Boolean = {
-      val points = hand.flatMap(labelPoints.get)
-      val otherPoints = other.hand.flatMap(labelPoints.get)
-      points.zip(otherPoints)
+    def getKindWithJoker(): Int = {
+      val numJokers = hand.count(_ == 'J')
+      CamelCard.getCounts(hand.filterNot(_ == 'J')) match {
+        case x :: xs => getKind(x + numJokers :: xs)
+        case Nil => getKind(numJokers :: Nil)
+      }
+    }
+
+    def isStronger(other: CamelCard, lookup: Map[Char, Int]): Boolean = {
+      hand.zip(other.hand)
         .dropWhile { case (l, r) => l == r }
         .headOption match {
-          case Some((l, r)) => l > r
+          case Some((l, r)) => lookup(l) > lookup(r)
           case None => false
         }
-    }
-
-    override def compare(that: CamelCard): Int = {
-      (this.getKind(), that.getKind()) match {
-        case (l, r) if l == r => {
-          if (this.isStronger(that)) 1 else -1
-        }
-        case (l, r) => l - r
-      }
     }
   }
 
@@ -47,16 +46,35 @@ object Day7 extends App {
     def apply(line: String): CamelCard = {
       val parts = line.split("\\s")
       CamelCard(parts(0).trim, parts(1).trim.toInt)
+
+    }
+    def getCounts(hand: String): List[Int] = {
+      hand.groupBy(identity).view
+        .mapValues(_.size)
+        .values.toList
+        .sorted.reverse
+    }
+
+    def sortWithJoker(card1: CamelCard, card2: CamelCard): Boolean = {
+      (card1.getKindWithJoker(), card2.getKindWithJoker()) match {
+        case (l, r) if l == r => card2.isStronger(card1, labelPointsWithJoker)
+        case (l, r) => l < r
+      }
+    }
+
+    def sortDefault(card1: CamelCard, card2: CamelCard): Boolean = {
+      (card1.getKind(), card2.getKind()) match {
+        case (l, r) if l == r => card2.isStronger(card1, labelPoints)
+        case (l, r) => l < r
+      }
     }
   }
 
   val file = getResourceFile("day7/input")
-  val lines = getLines(file)
 
-  val cards = lines.map(CamelCard(_)).toList.sorted
-  // debug
-  cards.foreach(println)
+  val cards = getLines(file).map(CamelCard(_)).toList.sortWith(CamelCard.sortDefault)
+  val cardsWithJoker = getLines(file).map(CamelCard(_)).toList.sortWith(CamelCard.sortWithJoker)
 
-  println(cards.length)
   println(cards.zipWithIndex.map { case (c, i) => c.bid * (i + 1) }.sum)
+  println(cardsWithJoker.zipWithIndex.map { case (c, i) => c.bid * (i + 1) }.sum)
 }
